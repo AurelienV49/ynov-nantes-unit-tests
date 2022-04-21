9662fb589b147c411c8567a784a170392019af97
Antonin <antonin.joulie@ynov.com>
2022-04-21 14:16:15 +0200

    test working in e2e-tests

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -2,4 +2,16 @@ Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+
+    //click on the research button
+    I.click({css: 'a.cta'});
+
+    //write in the input field the word 'info'
+    I.fillField({css: 'input.searchfield'}, 'info');
+
+    //wait a little to see the results
+    I.wait(5);
+    
+    //verify if we see "Bachelor Informatique" in the research results
+    I.see("Bachelor Informatique")
 });
