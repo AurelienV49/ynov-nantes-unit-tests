7196fae9386f55cd080fe858d12db52c29dcca09
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2022-04-21 16:39:27 +0200

    Tests unitaire TempTrackerTest

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -4,8 +4,44 @@ use PHPUnit\Framework\TestCase;
 
 require_once('./src/TempTracker.php');
 
-class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEqual(42, 42);
+class TempTrackerTest extends TestCase
+{
+    public function test_something()
+    {
+
+        $this->assertEquals(42, 42);
+    }
+
+    public function testFailure3()
+    {
+        $this->assertEquals("foo\nbah\nbaz\n", "foo\nbah\nbaz\n");
+    }
+
+    public function test_insert()
+    {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(45);
+        $this->assertEquals($tempTracker->get_temps(), [45]);
+    }
+
+    public function test_insert_outofbound()
+    {
+        $tempTracker = new TempTracker();
+        $this->expectException(ValueError::class);
+        $tempTracker->insert(-10);
+    }
+
+    public function test_insert_wrong_typing_1()
+    {
+        $tempTracker = new TempTracker();
+        $this->expectException(ValueError::class);
+        $this->expectException($tempTracker->insert("-10"));
+    }
+
+    public function test_insert_wrong_typing_2()
+    {
+        $tempTracker = new TempTracker();
+        $this->expectException(ValueError::class);
+        $this->expectException($tempTracker->insert("string"));
     }
 }
