8186b9f68902d246a09366eedb05e2e10e66793b
Antonin Joulie <antonin.joulie@ynov.com>
2022-04-29 10:32:42 +0200

    Mise en forme

--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -1,38 +1,38 @@
 const { MineSweeper } = require("../src/minesweeper");
-describe("MineSweeper", function() {
-    const minesweeper = new MineSweeper();
-    it("Input saisie dimension grille", function() {
-        const isGrilleOk = minesweeper.Input_dimensionsGrille();
-        expect(isGrilleOk).toBe(true);
-    });
-    it("Input saisie nombre de bombes dans interval", function() {
-        const isNbBombeOk = minesweeper.Input_nbBombe();
-        expect(isNbBombeOk).toBe(true);
-    });
-    it("Générer grille ", function() {
-        const isGrilleGenerationOk = minesweeper.GenererGrilleVide();
-        expect(isGrilleGenerationOk).not.toBeUndefined();;
-    });
-    it("Test si les bombes on bien été générées sur la grille ", function() {
-        const nbBombeSurLaGrille = minesweeper.GenererAjouterBombesAGrille();
-        expect(minesweeper.NbBombes).toEqual(nbBombeSurLaGrille);
-    });
-    it("'Suggested Test Cases' de l'exercice ", function() {
-        const minesweeper_2 = new MineSweeper();
-        const aaa = minesweeper_2.Input_dimensionsGrille_2(4, 4);
-        const inputGrid = [
-            ['*', '.', '.', '.'],
-            ['.', '.', '.', '.'],
-            ['.', '*', '.', '.'],
-            ['.', '.', '.', '.']
-        ];
-        const outputGrid_Attendu = [
-            ['*', '1', '0', '0'],
-            ['2', '2', '1', '0'],
-            ['1', '*', '1', '0'],
-            ['1', '1', '1', '0']
-        ];
-        const grilleGeneree = minesweeper_2.GenererGrille_2(inputGrid);
-        expect(grilleGeneree).toEqual(outputGrid_Attendu);
-    });
+describe("MineSweeper", function () {
+  const minesweeper = new MineSweeper();
+  it("Input saisie dimension grille", function () {
+    const isGrilleOk = minesweeper.Input_dimensionsGrille();
+    expect(isGrilleOk).toBe(true);
+  });
+  it("Input saisie nombre de bombes dans interval", function () {
+    const isNbBombeOk = minesweeper.Input_nbBombe();
+    expect(isNbBombeOk).toBe(true);
+  });
+  it("Générer grille ", function () {
+    const isGrilleGenerationOk = minesweeper.GenererGrilleVide();
+    expect(isGrilleGenerationOk).not.toBeUndefined();
+  });
+  it("Test si les bombes on bien été générées sur la grille ", function () {
+    const nbBombeSurLaGrille = minesweeper.GenererAjouterBombesAGrille();
+    expect(minesweeper.NbBombes).toEqual(nbBombeSurLaGrille);
+  });
+  it("'Suggested Test Cases' de l'exercice ", function () {
+    const minesweeper_2 = new MineSweeper();
+    const aaa = minesweeper_2.Input_dimensionsGrille_2(4, 4);
+    const inputGrid = [
+      ["*", ".", ".", "."],
+      [".", ".", ".", "."],
+      [".", "*", ".", "."],
+      [".", ".", ".", "."],
+    ];
+    const outputGrid_Attendu = [
+      ["*", "1", "0", "0"],
+      ["2", "2", "1", "0"],
+      ["1", "*", "1", "0"],
+      ["1", "1", "1", "0"],
+    ];
+    const grilleGeneree = minesweeper_2.GenererGrille_2(inputGrid);
+    expect(grilleGeneree).toEqual(outputGrid_Attendu);
+  });
 });
